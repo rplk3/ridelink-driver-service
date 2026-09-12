579d312a4392ea11f89bff81561e58ac8c78c58d
Ranuda Perera <it24100688@my.sliit.lk>
2026-09-12 18:04:28 +0530

    first commit

---
+++ src/main/java/com/example/ridelink_driver/RidelinkDriverServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.ridelink_driver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RidelinkDriverServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RidelinkDriverServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ridelink_driver/RidelinkDriverServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ridelink_driver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RidelinkDriverServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
